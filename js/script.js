a950a22f7571fc59cc9669f0ed3065a753dc189f
masonschratter <60018294+masonschratter@users.noreply.github.com>
2020-11-08 22:32:45 -0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+//$('creative').parallax({
+//    imageSrc: 'https://source.unsplash.com/1140x600/daily/?jacket'
+//});
+
+//header//
+$(".toggle").click(function(){
+    $('header').toggleClass('show-menu');
+ });
